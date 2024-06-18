3ff5d47d6e19fb642e205e29ff67ac960c9ba4ac
deisy03 <desiyesquivia03@gmail.com>
2024-06-18 10:00:38 -0600

    formulario

--- back/src/controladores/controladorProducto.js
+++ back/src/controladores/controladorProducto.js
@@ -1,103 +1,120 @@
+import multer from 'multer';
 import ModeloProducto from "../modelos/modeloProducto.js";
 
 const ControladorProductos = {
-    crearProducto: async (solicitud, respuesta) => {
-      try {
-       
-        const nuevoProducto = new ModeloProducto(solicitud.body);
-        const ProductoCreado = await nuevoProducto.save();
-        if (ProductoCreado._id) {
+  crearProducto: async (solicitud, respuesta) => {
+    try {
+      const almacenamiento = multer.diskStorage({
+        destination: 'imagenes',
+        filename: (req, file, cb) => {
+          cb(null, file.originalname);
+        },
+      });
+      const carga = multer({ storage: almacenamiento }).single('imagen');
+      carga(solicitud, respuesta, async (error) => {
+        if (error) {
           respuesta.json({
-            resultado: 'bien',
-            mensaje: 'Producto creado',
-            datos: ProductoCreado._id,
-          });
-        }
-      } catch (error) {
-        respuesta.json({
-          resultado: 'mal',
-          mensaje: 'ocurrió un error al crear Producto',
-          datos: error,
-        });
-      }
-    },
-    leerProducto: async (solicitud, respuesta) => {
-      try {
-        const ProductoEncontrado = await ModeloProducto.findById(
-          solicitud.params.id
-        );
-        if (ProductoEncontrado._id) {
-          respuesta.json({
-            resultado: 'bien',
-            mensaje: 'Producto leído',
-            datos: ProductoEncontrado,
-          });
-        }
-      } catch (error) {
-        respuesta.json({
-          resultado: 'mal',
-          mensaje: 'ocurrió un error al leer Producto',
-          datos: error,
-        });
-      }
-    },
-    leerProductos: async (solicitud, respuesta) => {
-      try {
-        const todosLosProductos = await ModeloProducto.find();
-        respuesta.json({
-          resultado: 'bien',
-          mensaje: 'Productos leídos',
-          datos: todosLosProductos,
-        });
-      } catch (error) {
-        respuesta.json({
-          resultado: 'mal',
-          mensaje: 'ocurrió un error al leer todos los Productos',
-          datos: error,
-        });
-      }
-    },
-    actualizarProducto: async (solicitud, respuesta) => {
-      try {
-        const ProductoActualizado = await ModeloProducto.findByIdAndUpdate(
-          solicitud.params.id,
-          solicitud.body
-        );
-        if (ProductoActualizado._id) {
-          respuesta.json({
-            resultado: 'bien',
-            mensaje: 'Producto actualizado',
-            datos: ProductoActualizado._id,
-          });
-        }
-      } catch (error) {
-        respuesta.json({
-          resultado: 'mal',
-          mensaje: 'ocurrió un error al actualizar Producto',
-          datos: error,
-        });
-      }
-    },
-    eliminarProducto: async (solicitud, respuesta) => {
-      try {
-        const ProductoEliminado = await ModeloProducto.findByIdAndDelete(
-          solicitud.params.id
-        );
-        if (ProductoEliminado._id) {
-          respuesta.json({
-            resultado: 'bien',
-            mensaje: 'Producto eliminado',
+            resultado: 'mal',
+            mensaje: 'ocurrió un error al subir imagen gorra',
             datos: null,
           });
+        } else {
+          const nuevaProductos = new ModeloProducto({
+            modelo: solicitud.body.modelo,
+            marca: solicitud.body.marca,
+            material: solicitud.body.material,
+            precio: solicitud.body.precio,
+            color: solicitud.body.color,
+            disponibilidad: solicitud.body.disponibilidad,
+            fechaIngreso: solicitud.body.fechaIngreso,
+            imagen: {
+              data: solicitud.file.filename,
+              contentType: 'image/png',
+            },
+          });
+          const ProductosCreada = await nuevaProductos.save();
+          if (ProductosCreada._id) {
+            respuesta.json({
+              resultado: 'bien',
+              mensaje: 'Productos creada',
+              datos: ProductosCreada._id,
+            });
+          }
         }
-      } catch (error) {
-        respuesta.json({
-          resultado: 'mal',
-          mensaje: 'ocurrió un error al eliminar Producto',
-          datos: error,
-        });
-      }
-    },
-  };
+      });
+    } catch (error) {
+      respuesta.json({
+        resultado: 'mal',
+        mensaje: 'ocurrió un error al crear Productos',
+        datos: error,
+      });
+    }
+  },
+  leerProducto: async (solicitud, respuesta) => {
+    try {
+      respuesta.json({
+        resultado: 'bien',
+        mensaje: 'Producto leído',
+        datos: null,
+      });
+    
+    } catch (error) {
+      respuesta.json({
+        resultado: 'mal',
+        mensaje: 'ocurrió un error al leer Producto',
+        datos: error,
+      });
+    }
+  },
+  leerProductos: async (solicitud, respuesta) => {
+    try {
+      respuesta.json({
+        resultado: 'bien',
+        mensaje: 'Productos leídos',
+        datos: null,
+      });
+     
+    } catch (error) {
+      respuesta.json({
+        resultado: 'mal',
+        mensaje: 'ocurrió un error al leer todas los Productos',
+        datos: error,
+      });
+    }
+  },
+  actualizarProducto: async (solicitud, respuesta) => {
+    try {
+      respuesta.json({
+        resultado: 'bien',
+        mensaje: 'Productos actualizados',
+        datos: null,
+      });
+  
+    } catch (error) {
+      respuesta.json({
+        resultado: 'mal',
+        mensaje: 'ocurrió un error al actualizar Productos',
+        datos: error,
+      });
+    }
+  },
+  eliminarProducto: async (solicitud, respuesta) => {
+    try {
+      respuesta.json({
+        resultado: 'bien',
+        mensaje: 'Productos eliminado',
+        datos: null,
+      });
+     
+    } catch (error) {
+      respuesta.json({
+        resultado: 'mal',
+        mensaje: 'ocurrió un error al eliminar Productos',
+        datos: error,
+      });
+    }
+  },
+};
+
   
   export default ControladorProductos;
--- back/src/modelos/modeloProducto.js
+++ back/src/modelos/modeloProducto.js
@@ -3,14 +3,14 @@ import  {Schema, model} from "mongoose";
 const esquemaProducto = new Schema({
 
     modelo: { type: String, required: true },
-    imagen:{type: String, required:true},
     marca: { type: String, required: true },
     material:{ type: String, required: true },
     precio: { type: Number, required: true },
     color:{ type: String, required: true },
     disponibilidad:{ type: Boolean, required: true },
     fechaIngreso:{ type: Date, required: true },
-     
-
-      });
+    imagen:{date: Buffer, contentType:String},
+},
+ { versionKey: false, timestamps: true }
+  );
       export default model("producto",esquemaProducto);
