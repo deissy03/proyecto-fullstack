0fc0aa9d68a721d9dd87f3aba4557eca60b800b2
deisy03 <desiyesquivia03@gmail.com>
2024-07-30 18:23:37 -0600

    pagina de bolsos

--- front/src/app/service2/data.service.ts
+++ front/src/app/service2/data.service.ts
@@ -11,7 +11,7 @@ export class DataService {
   //otro metodo//
   async getDatos(): Promise<Ejemplo[]> {
     return await firstValueFrom(
-      this.http.get<any>(`http://http://3.133.98.196:3000/productos/`).pipe(map((DataService) => DataService.datos as Ejemplo[]))
+      this.http.get<any>(`http://3.133.98.196:3000/productos/`).pipe(map((DataService) => DataService.datos as Ejemplo[]))
     );
   }
   
